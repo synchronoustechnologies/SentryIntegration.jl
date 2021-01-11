69948985094e1c2f77b92188d9dca22d0a4e3a8e
Daniel Cocks <daniel.cocks@gmail.com>
2021-01-11 13:31:32 +1100

    Piggy-back off python

---
+++ src/SentryIntegration.jl
@@ -0,0 +1,31 @@
+module SentryIntegration
+
+# Currently a dodgy wrapper around the python implementation
+
+using PyCall
+using Logging: Info, Warn, Error
+
+export sentry_message,
+    Info,
+    Warn,
+    Error
+
+# Technically this isn't needed.
+function should_sentry()
+    run_env = get(ENV, "RUN_ENV", "DEV")
+    return uppercase(run_env) ∈ ["STAGE", "PRODUCTION"]
+end
+
+const sentry_initialised = Ref(false)
+function sentry_message(message, level=Info)
+    should_sentry() || return
+    sentry_sdk = pyimport("sentry_sdk")
+    if !sentry_initialised[]
+        sentry_sdk.init()
+        sentry_initialised[] = true
+    end
+
+    sentry_sdk.capture_message(message)
+end
+
+end # module
