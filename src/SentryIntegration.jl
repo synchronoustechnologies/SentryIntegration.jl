3a8020ab70dd4abc5c3a1f2c5d6d798811fc3002
Daniel Cocks <daniel.cocks@gmail.com>
2021-01-15 15:49:57 +1100

    Split off the init and allow setting the dsn internally

--- src/SentryIntegration.jl
+++ src/SentryIntegration.jl
@@ -6,9 +6,14 @@ using PyCall
 using Logging: Info, Warn, Error
 
 export sentry_message,
+    set_sentry_dsn,
     Info,
     Warn,
     Error
+
+const override_url = Ref{String}("")
+
+set_sentry_dsn(s) = override_url[] = s
 
 # Technically this isn't needed.
 function should_sentry()
@@ -16,16 +21,27 @@ function should_sentry()
     return uppercase(run_env) ∈ ["STAGE", "PRODUCTION"]
 end
 
+function maybe_init()
+    if !sentry_initialised[]
+        sentry_sdk = pyimport("sentry_sdk")
+        @info "init sentry" dsn=override_url[]
+        if override_url[] == ""
+            sentry_sdk.init()
+        else
+            sentry_sdk.init(dsn=override_url[])
+        end
+        sentry_initialised[] = true
+    end
+end
+
 const sentry_initialised = Ref(false)
 function sentry_message(message, level=Info)
     should_sentry() || return
     try
+        maybe_init()
+        
+        @info "sending sentry message" message level
         sentry_sdk = pyimport("sentry_sdk")
-        if !sentry_initialised[]
-            sentry_sdk.init()
-            sentry_initialised[] = true
-        end
-
         sentry_sdk.capture_message(message)
     catch exc
         @error "Ignoring problem in sentry message" exc
